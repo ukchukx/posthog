a585803b382b053f29fba8ab8b0a753396c23fa7
Marcus <marcus.hoile@3plearning.com>
2025-03-08 16:08:10 +1100

    update url in test stub

--- test/support/hackney_stub.ex
+++ test/support/hackney_stub.ex
@@ -1,7 +1,7 @@
 defmodule HackneyStub do
   @fixtures_dir Path.join(__DIR__, "fixtures")
 
-  def post("https://app.posthog.com/decide?v=3", _headers, _body, _opts) do
+  def post("https://us.posthog.com/decide?v=3", _headers, _body, _opts) do
     {:ok, 200, json_fixture!("decide.json"), "decide"}
   end
 
