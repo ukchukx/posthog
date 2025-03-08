c2492a712b4322f11e1212cf06da8b555e7f54bb
Marcus <marcus.hoile@gmail.com>
2025-03-08 15:51:42 +1100

    use fake key in test config
    Co-authored-by: Rafael Audibert <32079912+rafaeelaudibert@users.noreply.github.com>

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,5 @@
 import Config
 
 if config_env() == :test do
-  config :posthog, api_key: "phc_yeluS6373YhTTtwXEhRZ5vEHKyVIsTkm2HGtEwbMr4D", api_url: "https://app.posthog.com"
+  config :posthog, api_key: "phc_randomrandomrandom", api_url: "https://us.posthog.com"
 end
