1298008505b0da8e4cc2ae0da61499e1c7d2f245
Phil Haack <haacked@gmail.com>
2025-04-16 16:55:08 -0700

    Update test names

--- test/posthog_test.exs
+++ test/posthog_test.exs
@@ -2,86 +2,9 @@ defmodule PosthogTest do
   use ExUnit.Case, async: true
   import Mimic
 
-  describe "feature_flag/4" do
-    test "when feature flag exists, returns feature flag struct" do
-      stub_with(:hackney, HackneyStub)
-
-      assert Posthog.feature_flag("my-awesome-flag", "user_123") ==
-               {:ok,
-                %Posthog.FeatureFlag{
-                  enabled: true,
-                  name: "my-awesome-flag",
-                  payload: "example-payload-string"
-                }}
-    end
-
-    test "when feature flag has a json payload, will return decoded payload" do
-      stub_with(:hackney, HackneyStub)
-
-      assert Posthog.feature_flag("my-awesome-flag-2", "user_123") ==
-               {:ok,
-                %Posthog.FeatureFlag{
-                  enabled: true,
-                  name: "my-awesome-flag-2",
-                  payload: %{"color" => "blue", "animal" => "hedgehog"}
-                }}
-    end
-
-    test "when feature flag has an array payload, will return decoded payload" do
-      stub_with(:hackney, HackneyStub)
-
-      assert Posthog.feature_flag("array-payload", "user_123") ==
-               {:ok,
-                %Posthog.FeatureFlag{
-                  enabled: true,
-                  name: "array-payload",
-                  payload: [0, 1, 2]
-                }}
-    end
-
-    test "when feature flag does not have a payload, will return flag value" do
-      stub_with(:hackney, HackneyStub)
-
-      assert Posthog.feature_flag("flag-thats-not-on", "user_123") ==
-               {:ok,
-                %Posthog.FeatureFlag{
-                  enabled: false,
-                  name: "flag-thats-not-on",
-                  payload: nil
-                }}
-    end
-
-    test "when feature flag does not exist, returns not_found" do
-      stub_with(:hackney, HackneyStubV3)
-
-      assert Posthog.feature_flag("does-not-exist", "user_123") ==
-               {:error, :not_found}
-    end
-  end
-
-  describe "feature_flag_enabled?/4" do
-    test "true if the feature flag is enabled" do
-      stub_with(:hackney, HackneyStub)
-
-      assert Posthog.feature_flag_enabled?("my-awesome-flag", "user_123")
-    end
-
-    test "false if the feature flag is disabled" do
-      stub_with(:hackney, HackneyStub)
-
-      refute Posthog.feature_flag_enabled?("flag-thats-not-on", "user_123")
-    end
-
-    test "false if the feature flag does not exist" do
-      stub_with(:hackney, HackneyStub)
-
-      refute Posthog.feature_flag_enabled?("flag-does-not-exist", "user_123")
-    end
-  end
-
   describe "feature_flag/3" do
     test "when feature flag exists, returns feature flag struct" do
-      stub_with(:hackney, HackneyStubV3)
+      stub_with(:hackney, HackneyStub)
 
       assert Posthog.feature_flag("my-awesome-flag", "user_123") ==
                {:ok,
@@ -93,7 +16,7 @@ defmodule PosthogTest do
     end
 
     test "when feature flag has a json payload, will return decoded payload" do
-      stub_with(:hackney, HackneyStubV3)
+      stub_with(:hackney, HackneyStub)
 
       assert Posthog.feature_flag("my-awesome-flag-2", "user_123") ==
                {:ok,
@@ -105,7 +28,7 @@ defmodule PosthogTest do
     end
 
     test "when feature flag has an array payload, will return decoded payload" do
-      stub_with(:hackney, HackneyStubV3)
+      stub_with(:hackney, HackneyStub)
 
       assert Posthog.feature_flag("array-payload", "user_123") ==
                {:ok,
@@ -117,7 +40,7 @@ defmodule PosthogTest do
     end
 
     test "when feature flag does not have a payload, will return flag value" do
-      stub_with(:hackney, HackneyStubV3)
+      stub_with(:hackney, HackneyStub)
 
       assert Posthog.feature_flag("flag-thats-not-on", "user_123") ==
                {:ok,
@@ -137,6 +60,83 @@ defmodule PosthogTest do
   end
 
   describe "feature_flag_enabled?/3" do
+    test "true if the feature flag is enabled" do
+      stub_with(:hackney, HackneyStub)
+
+      assert Posthog.feature_flag_enabled?("my-awesome-flag", "user_123")
+    end
+
+    test "false if the feature flag is disabled" do
+      stub_with(:hackney, HackneyStub)
+
+      refute Posthog.feature_flag_enabled?("flag-thats-not-on", "user_123")
+    end
+
+    test "false if the feature flag does not exist" do
+      stub_with(:hackney, HackneyStub)
+
+      refute Posthog.feature_flag_enabled?("flag-does-not-exist", "user_123")
+    end
+  end
+
+  describe "v3 - feature_flag/3" do
+    test "when feature flag exists, returns feature flag struct" do
+      stub_with(:hackney, HackneyStubV3)
+
+      assert Posthog.feature_flag("my-awesome-flag", "user_123") ==
+               {:ok,
+                %Posthog.FeatureFlag{
+                  enabled: true,
+                  name: "my-awesome-flag",
+                  payload: "example-payload-string"
+                }}
+    end
+
+    test "when feature flag has a json payload, will return decoded payload" do
+      stub_with(:hackney, HackneyStubV3)
+
+      assert Posthog.feature_flag("my-awesome-flag-2", "user_123") ==
+               {:ok,
+                %Posthog.FeatureFlag{
+                  enabled: true,
+                  name: "my-awesome-flag-2",
+                  payload: %{"color" => "blue", "animal" => "hedgehog"}
+                }}
+    end
+
+    test "when feature flag has an array payload, will return decoded payload" do
+      stub_with(:hackney, HackneyStubV3)
+
+      assert Posthog.feature_flag("array-payload", "user_123") ==
+               {:ok,
+                %Posthog.FeatureFlag{
+                  enabled: true,
+                  name: "array-payload",
+                  payload: [0, 1, 2]
+                }}
+    end
+
+    test "when feature flag does not have a payload, will return flag value" do
+      stub_with(:hackney, HackneyStubV3)
+
+      assert Posthog.feature_flag("flag-thats-not-on", "user_123") ==
+               {:ok,
+                %Posthog.FeatureFlag{
+                  enabled: false,
+                  name: "flag-thats-not-on",
+                  payload: nil
+                }}
+    end
+
+    test "when feature flag does not exist, returns not_found" do
+      stub_with(:hackney, HackneyStubV3)
+
+      assert Posthog.feature_flag("does-not-exist", "user_123") ==
+               {:error, :not_found}
+    end
+  end
+
+  describe "v3 - feature_flag_enabled?/3" do
     test "true if the feature flag is enabled" do
       stub_with(:hackney, HackneyStubV3)
 
