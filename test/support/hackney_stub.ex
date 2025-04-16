7980c6514fcbecd880726bcf431c4d0e411dd6e1
Phil Haack <haacked@gmail.com>
2025-04-16 14:26:26 -0700

    Add tests using v4 response

--- test/support/hackney_stub.ex
+++ test/support/hackney_stub.ex
@@ -1,4 +1,4 @@
-defmodule HackneyStub do
+defmodule HackneyStubV3 do
   @fixtures_dir Path.join(__DIR__, "fixtures")
 
   def post("https://us.posthog.com/decide?v=3", _headers, _body, _opts) do
@@ -7,6 +7,24 @@ defmodule HackneyStub do
 
   def body("decide") do
     {:ok, json_fixture!("decide.json")}
+  end
+
+  defp json_fixture!(fixture) do
+    @fixtures_dir
+    |> Path.join(fixture)
+    |> File.read!()
+  end
+end
+
+defmodule HackneyStub do
+  @fixtures_dir Path.join(__DIR__, "fixtures")
+
+  def post("https://us.posthog.com/decide?v=3", _headers, _body, _opts) do
+    {:ok, 200, json_fixture!("decide-v3.json"), "decide"}
+  end
+
+  def body("decide") do
+    {:ok, json_fixture!("decide-v3.json")}
   end
 
   defp json_fixture!(fixture) do
