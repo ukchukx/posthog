6ad6aa23186a78d6228b500c2b77872fcc651a25
Phil Haack <haacked@gmail.com>
2025-04-16 21:39:15 -0700

    Change to LRU

--- lib/posthog/application.ex
+++ lib/posthog/application.ex
@@ -7,7 +7,7 @@ defmodule Posthog.Application do
     children = [
       # Start Cachex for feature flag event deduplication.
       # The 50,000 entries limit is the same used for posthog-python, but otherwise arbitrary.
-      {Cachex, name: :posthog_feature_flag_cache, limit: 50_000, policy: Cachex.Policy.LRW}
+      {Cachex, name: :posthog_feature_flag_cache, limit: 50_000, policy: Cachex.Policy.LRU}
     ]
 
     opts = [strategy: :one_for_one, name: Posthog.Supervisor]
